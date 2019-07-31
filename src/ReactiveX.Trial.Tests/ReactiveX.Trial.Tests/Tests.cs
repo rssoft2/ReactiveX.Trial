ede9f185870b8736651f7c5451506c2c63700d06
Rabold, Frank <frank.rabold@klingelnberg.com>
2019-07-31 12:49:21 +0200

    add ClassicHotlink as observable use case

--- src/ReactiveX.Trial.Tests/ReactiveX.Trial.Tests/Tests.cs
+++ src/ReactiveX.Trial.Tests/ReactiveX.Trial.Tests/Tests.cs
@@ -21,8 +21,6 @@ namespace ReactiveX.Trial.Tests
             var subscription = observable.Subscribe(Console.Write);
 
             subscription.Dispose();
-
-            Assert.Pass();
         }
 
         [Test]
@@ -39,8 +37,6 @@ namespace ReactiveX.Trial.Tests
             subject.OnNext('b');
 
             subscription.Dispose();
-
-            Assert.Pass();
         }
 
         [Test]
@@ -50,8 +46,6 @@ namespace ReactiveX.Trial.Tests
 
             var subscription = observable.Subscribe(Console.WriteLine);
             subscription.Dispose();
-
-            Assert.Pass();
         }
 
         [Test]
@@ -63,9 +57,8 @@ namespace ReactiveX.Trial.Tests
                 IObservable<string> observableData = data;
                 IObservable<string> observableProcess = process;
 
-                var observable = observableData.WithLatestFrom(observableProcess, (data1, process1) => data1 + process1);
-                //var observable = observableData.Merge(observableProcess);
-                //var observable = observableData.Zip(observableProcess, (data1, process1) => data1 + process1);
+                var observable =
+                    observableData.WithLatestFrom(observableProcess, (data1, process1) => data1 + process1);
 
                 const string expected = "2a3a4a5a6b7b8b";
                 var actual = string.Empty;
@@ -101,6 +94,61 @@ namespace ReactiveX.Trial.Tests
         {
             for (var i = 0; i < 10; i++) observer.OnNext(i + 1);
             return Disposable.Empty;
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var classicHotlink = new ClassicHotlink<string>();
+
+            void CallbackAction(string value)
+            {
+                Console.Write(value);
+            }
+
+            void DisposedAction()
+            {
+                Console.WriteLine(" classic hotlink disposed");
+            }
+
+            var observable = classicHotlink.AsObservable(DisposedAction);
+            var subscription = observable.Subscribe(CallbackAction);
+
+            classicHotlink.RaiseCallback("1");
+            classicHotlink.RaiseCallback("2");
+            classicHotlink.RaiseCallback("3");
+            classicHotlink.RaiseCallback(" der gejid!");
+
+            subscription.Dispose();
+        }
+    }
+
+    public interface IClassicHotlink<out T>
+    {
+        IDisposable CreateHotlinkSingle(Action<T> callback, Action disposed);
+    }
+
+    public class ClassicHotlink<T> : IClassicHotlink<T>
+    {
+        private Action<T> _callback;
+
+        public IDisposable CreateHotlinkSingle(Action<T> callback, Action disposed)
+        {
+            _callback = callback;
+            return Disposable.Create(disposed);
+        }
+
+        public void RaiseCallback(T value)
+        {
+            _callback(value);
+        }
+    }
+
+    public static class ObservableExtensions
+    {
+        public static IObservable<T> AsObservable<T>(this IClassicHotlink<T> classicHotlink, Action disposed)
+        {
+            return Observable.Create<T>(observer => classicHotlink.CreateHotlinkSingle(observer.OnNext, disposed));
         }
     }
 }
